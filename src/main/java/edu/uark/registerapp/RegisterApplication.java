3d3ce17b99a420c2f1c569909ec20b0e8445c1fc
Evie Smith <joetam@uark.edu>
2020-10-01 15:15:37 -0500

    new commit

--- src/main/java/edu/uark/registerapp/RegisterApplication.java
+++ src/main/java/edu/uark/registerapp/RegisterApplication.java
@@ -11,3 +11,5 @@ public class RegisterApplication {
 	}
 
 }
+//eviefffff
+//yeet
--- src/main/java/edu/uark/registerapp/models/api/Employee.java
+++ src/main/java/edu/uark/registerapp/models/api/Employee.java
@@ -31,12 +31,99 @@ public class Employee extends ApiResponse{
         this.isInitialEmployee = false;
     }
 
+    // Get Functions
+
     public UUID getID() {
         return this.id;
     }
+
+    public String getEmployeeID() {
+        return this.employeeID;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public boolean getIsActive() {
+        return this.isActive;
+    }
+
+    public int getClassification() {
+        return this.classification;
+    }
+
+    public UUID getManagerID() {
+        return this.managerID;
+    }
+
+    public String getCreatedOn() {
+        return this.createdOn;
+    }
+
+    public boolean getIsInitialEmployee() {
+        return this.isInitialEmployee;
+    }
+
+    // Set Functions
 
     public Employee setID(final UUID id) {
         this.id = id;
         return this;
     }
+
+    public Employee setEmployeeID(final String employeeID) {
+        this.employeeID = employeeID;
+        return this;
+    }
+
+    public Employee setFirstName(final String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public Employee setLastName(final String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public Employee setPassword(final String password) {
+        this.password = password;
+        return this;
+    }
+
+    public Employee setIsActive(final boolean isActive) {
+        this.isActive = isActive;
+        return this;
+
+    }
+
+    public Employee setClassification(final int classification) {
+        this.classification = classification
+        return this;
+    }
+
+    public Employee setManagerID(final UUID managerID) {
+        this.managerID = managerID;
+        return this;
+    }
+
+    public Employee setCreatedOn(final String createdOn) {
+        this.createdOn = createdOn;
+        return this;
+    }
+
+    public Employee setIsInitialEmployee(final boolean isInitialEmployee) {
+        this.isInitialEmployee = isInitialEmployee;
+        return this;
+    }
+
 }
