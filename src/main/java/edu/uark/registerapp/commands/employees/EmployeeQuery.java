65f3c17872911bcce4156f88d275bc5d2a5bfec2
notjoetam <49158108+notjoetam@users.noreply.github.com>
2020-10-07 21:53:12 -0500

    Add files via upload

---
+++ src/main/java/edu/uark/registerapp/commands/employees/EmployeeQuery.java
@@ -0,0 +1,35 @@
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.uark.registerapp.commands.ResultCommandInterface;
+import edu.uark.registerapp.commands.exceptions.NotFoundException;
+import edu.uark.registerapp.models.api.Employee;
+import edu.uark.registerapp.models.entities.EmployeeEntity;
+import edu.uark.registerapp.models.repositories.EmployeeRepository;
+
+public class EmployeeQuery implements ResultCommandInterface<Employee>{
+    public Employee execute(){
+        EmployeeEntity employeeEntity = this.employeeRespoitory.findById(this.employeeId);
+        if(employeeEntity.isPresent()){
+            return new Employee(employeeEntity.get());
+        }
+        else{
+            throw new NotFoundException("Employee");
+        }
+    }
+
+    // Properties
+    private UUID employeeId;
+    public UUID getEmployeeID(){
+        return this.employeeId;
+    }
+    public EmployeeQuery setEmployeeId(final UUID employeeId){
+        this.employeeId = employeeId;
+        return this;
+    }
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+}
---
+++ src/main/java/edu/uark/registerapp/commands/employees/EmployeeUpdateCommand.java
@@ -0,0 +1,67 @@
+import java.util.UUID;
+
+import javax.transaction.Transactional;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.uark.registerapp.commands.ResultCommandInterface;
+import edu.uark.registerapp.commands.exceptions.NotFoundException;
+import edu.uark.registerapp.commands.exceptions.UnprocessableEntityException;
+import edu.uark.registerapp.models.api.Employee;
+import edu.uark.registerapp.models.entities.EmployeeEntity;
+import edu.uark.registerapp.models.enums.EmployeeClassification;
+import edu.uark.registerapp.models.repositories.EmployeeRepository;
+
+public class EmployeeUpdateCommand {
+    public Employee execute(){
+        this.validProperties();
+        this.updateEmployeeEntity();
+        return this.apiEmployee;
+    }
+
+    // Helper Methods
+    private void validProperties(){
+        if(StringUtils.isBlank(this.apiEmployee.getFirstName())){
+            throw new UnprocessableEntityException("First Name");
+        }
+        if(StringUtils.isBlank(this.apiEmployee.getLastName())){
+            throw new UnprocessableEntityException("Last Name");
+        }
+        if(EmployeeClassification.map(this.apiEmployee.getClassification()) == EmployeeClassification.NOT_DEFINED){
+            throw new UnprocessableEntityException("Classification");
+        }
+    }
+
+    private void updateEmployeeEntity(){
+        EmployeeEntity quriedEmployeeEntity = this.employeeRepository.findbyId(this.employeeId);
+        if(!queriedEmployeeEntity.isPresent()){
+            throw new NotFoundException("Employee"); // No record with the assoicated record ID exists in the database
+        }
+
+        this.apiEmployee = quriedEmployeeEntity.get().synchronize(this.apiEmployee); // Synchronize any incoming changes for UPDATE to the database.
+        this.employeeRepository.save(quriedEmployeeEntity.get()); // Write, via an UPDATE, any changes to the database.
+    }
+
+    // Properties
+    private UUID employeeId;
+    public UUID getEmployeeId(){
+        return this.employeeId;
+    }
+    public EmployeeUpdateCommand setEmployeeId(final UUID employeeId){
+        this.employeeId = employeeId;
+        return this;
+    }
+
+    private Employee apiEmployee;
+    public Employee getApiEmployee(){
+        return this.apiEmployee;
+    }
+    public EmployeeUpdateCommand setApiEmployee(final Employee apiEmployee){
+        this.apiEmployee = apiEmployee;
+        return this;
+    }
+
+    private EmployeeRepository employeeRepository;
+}
