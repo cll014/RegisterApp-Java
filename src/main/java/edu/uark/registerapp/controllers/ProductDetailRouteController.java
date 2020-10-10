a57533e8fcdd01c1896c5605694e9240e06e3157
Evelyn Smith <evelynsmith028@gmail.com>
2020-10-10 01:47:22 -0500

    New Commit

--- src/main/java/edu/uark/registerapp/controllers/ProductDetailRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/ProductDetailRouteController.java
@@ -1,13 +1,18 @@
 package edu.uark.registerapp.controllers;
 
+import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
+import edu.uark.registerapp.models.entities.ActiveUserEntity;
+import edu.uark.registerapp.models.enums.EmployeeClassification;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import edu.uark.registerapp.commands.products.ProductQuery;
@@ -15,38 +20,59 @@ import edu.uark.registerapp.controllers.enums.ViewModelNames;
 import edu.uark.registerapp.controllers.enums.ViewNames;
 import edu.uark.registerapp.models.api.Product;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping(value = "/productDetail")
-public class ProductDetailRouteController {
+public class ProductDetailRouteController extends BaseRouteController{
 	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView start() {
-		return (new ModelAndView(ViewNames.PRODUCT_DETAIL.getViewName()))
-			.addObject(
-				ViewModelNames.PRODUCT.getValue(),
-				(new Product()).setLookupCode(StringUtils.EMPTY).setCount(0));
+	public ModelAndView start(@RequestParam Map<String, String> queryParameters, HttpServletRequest request) {
+		Optional<ActiveUserEntity> user = this.getCurrentUser(request);
+
+		if(!user.isPresent()) {
+			return this.buildInvalidSessionResponse();
+		}
+		else if (!isElevatedUser(user.get())){
+			return this.buildNoPermissionsResponse(ViewNames.PRODUCT_LISTING.getRoute());
+		}
+		else {
+			ModelAndView modelAndView = this.setErrorMessageFromQueryString(new ModelAndView(ViewNames.PRODUCT_DETAIL.getViewName()), queryParameters);
+			modelAndView.addObject(ViewModelNames.IS_ELEVATED_USER.getValue(), true);
+			modelAndView.addObject(ViewModelNames.PRODUCT.getValue(), (new Product()).setLookupCode(StringUtils.EMPTY).setCount(0));
+
+			return modelAndView;
+		}
 	}
 
 	@RequestMapping(value = "/{productId}", method = RequestMethod.GET)
-	public ModelAndView startWithProduct(@PathVariable final UUID productId) {
-		final ModelAndView modelAndView =
-			new ModelAndView(ViewNames.PRODUCT_DETAIL.getViewName());
+	public ModelAndView startWithProduct(@PathVariable final UUID productId, @RequestParam Map<String, String> queryParameters, HttpServletRequest request) {
+		Optional<ActiveUserEntity> user = this.getCurrentUser(request);
 
-		try {
-			modelAndView.addObject(
-				ViewModelNames.PRODUCT.getValue(),
-				this.productQuery.setProductId(productId).execute());
-		} catch (final Exception e) {
-			modelAndView.addObject(
-				ViewModelNames.ERROR_MESSAGE.getValue(),
-				e.getMessage());
-			modelAndView.addObject(
-				ViewModelNames.PRODUCT.getValue(),
-				(new Product())
-					.setCount(0)
-					.setLookupCode(StringUtils.EMPTY));
+
+		if (!user.isPresent()) {
+			return this.buildInvalidSessionResponse();
+		} else {
+			ModelAndView modelAndView = this.setErrorMessageFromQueryString(new ModelAndView(ViewNames.PRODUCT_DETAIL.getViewName()), queryParameters);
+			modelAndView.addObject(ViewModelNames.IS_ELEVATED_USER.getValue(), EmployeeClassification.isElevatedUser(user.get().getClassification()));
+
+			try {
+				modelAndView.addObject(
+						ViewModelNames.PRODUCT.getValue(),
+						this.productQuery.setProductId(productId).execute());
+			} catch (final Exception e) {
+				modelAndView.addObject(
+						ViewModelNames.ERROR_MESSAGE.getValue(),
+						e.getMessage());
+				modelAndView.addObject(
+						ViewModelNames.PRODUCT.getValue(),
+						(new Product())
+								.setCount(0)
+								.setLookupCode(StringUtils.EMPTY));
+			}
+
+			return modelAndView;
+
 		}
-
-		return modelAndView;
 	}
 
 	// Properties
