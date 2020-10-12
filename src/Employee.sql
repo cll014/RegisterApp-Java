3e669bc8b658ed208aa72c0c9fb6ccb52fc481ef
cll014 <71350838+cll014@users.noreply.github.com>
2020-10-11 22:48:17 -0500

    Update Employee.sql

--- src/Employee.sql
+++ src/Employee.sql
@@ -1,13 +1,13 @@
-CREATE TABLE employee (
-  record_id uuid NOT NULL DEFAULT gen_random_uuid(),
-  employee_id int NOT NULL DEFAULT(0),
-  first_name Char(128) NOT NULL DEFAULT(''),
-  last_name Char(128) NOT NULL DEFAULT(''),
+REATE TABLE employee (
+  id uuid NOT NULL DEFAULT gen_random_uuid(),
+  employeeid int NOT NULL DEFAULT(0),
+  firstname character varying(128) NOT NULL DEFAULT(''),
+  lastname character varying(128) NOT NULL DEFAULT(''),
   password bytea NOT NULL DEFAULT(''),
-  active boolean NOT NULL DEFAULT(FALSE),
+  active boolean NOT NULL DEFAULT(FALSE), 
   classification int NOT NULL DEFAULT(0),
-  classification Char NOT NULL CHECK(classification in ('general manager', 'shift manager', 'cashier')),
-  created_On timestamp without time zone NOT NULL DEFAULT now(),
+  managerid uuid NOT NULL DEFAULT CAST('00000000-0000-0000-0000-000000000000' AS uuid),
+  createdon timestamp without time zone NOT NULL DEFAULT now(),
   CONSTRAINT employee_pkey PRIMARY KEY (id)
 ) WITH (
   OIDS=FALSE
@@ -17,29 +17,29 @@ CREATE TABLE employee (
 -- how many employees are in your company. Obviously this is not foolproof.
 CREATE SEQUENCE employee_employeeid_seq AS int START WITH 253 INCREMENT BY 7 OWNED BY employee.employeeid;
 
-ALTER TABLE employee ALTER employee_id SET DEFAULT nextval('employee_employeeid_seq');
+ALTER TABLE employee ALTER employeeid SET DEFAULT nextval('employee_employeeid_seq');
 
 CREATE INDEX ix_employee_employeeid
   ON employee
-  USING btree(employee_id);
+  USING btree(employeeid);
 
 -----
-CREATE TABLE Active_User (
-  record_id uuid NOT NULL DEFAULT gen_random_uuid(),
-  employee_id uuid NOT NULL,
-  name Char(256) NOT NULL DEFAULT(''),
-  classification Char NOT NULL CHECK(classification IN ('general manager', 'shift manager', 'cashier')),
-  session_key Char(128) NOT NULL DEFAULT(''),
-  created_On timestamp without time zone NOT NULL DEFAULT now(),
-  CONSTRAINT activeUser_pkey PRIMARY KEY (record_id)
+CREATE TABLE activeuser (
+  id uuid NOT NULL DEFAULT gen_random_uuid(),
+  employeeid uuid NOT NULL,
+  name character varying(256) NOT NULL DEFAULT(''),
+  classification int NOT NULL DEFAULT(0),
+  sessionkey character varying (128) NOT NULL DEFAULT(''),
+  createdon timestamp without time zone NOT NULL DEFAULT now(),
+  CONSTRAINT activeuser_pkey PRIMARY KEY (id)
 ) WITH (
   OIDS=FALSE
 );
 
 CREATE INDEX ix_activeuser_employeeid
-  ON active_User
-  USING btree(employee_id);
+  ON activeuser
+  USING btree(employeeid);
 
 CREATE INDEX ix_activeuser_sessionkey
-  ON active_User
-  USING btree(session_key);
+  ON activeuser
+  USING btree(sessionkey);
